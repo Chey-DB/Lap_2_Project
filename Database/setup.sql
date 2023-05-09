362095b1c9bd5c133956aec4b383c45613fc06f1
George Hill <georgeophill@outlook.com>
2023-05-09 12:03:58 +0100

    edits to setup.sql file

--- Database/setup.sql
+++ Database/setup.sql
@@ -6,12 +6,14 @@ CREATE TABLE skill (
     skill_id INT GENERATED ALWAYS AS IDENTITY,
     title VARCHAR (100) NOT NULL,
     description VARCHAR (500) NOT NULL,
+    location VARCHAR (100) NOT NULL,
     PRIMARY KEY (skill_id)
 );
 
 CREATE TABLE user_account (
     user_id INT GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(30) UNIQUE NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
     password CHAR(60) NOT NULL,
     PRIMARY KEY (user_id)
 );
@@ -23,3 +25,5 @@ CREATE TABLE token (
     PRIMARY KEY (token_id),
     FOREIGN KEY (user_id) REFERENCES user_account("user_id")
 );
+
+-- INSERT INTO Skill VALUES (1,'Carpentry','Carpentry is a skilled trade and a craft in which the primary work performed is the cutting, shaping and installation of building materials during the construction of buildings, ships, timber bridges, concrete formwork,','Construction', 'Florin Close, Coventry, West Midlands, CV4');
