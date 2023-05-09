c38f3536af4c1e49762499f46467c27c85ca9af1
George Hill <georgeophill@outlook.com>
2023-05-09 12:26:51 +0100

    pluralising tables

--- Database/setup.sql
+++ Database/setup.sql
@@ -1,17 +1,17 @@
-DROP TABLE IF EXISTS skill;
-DROP TABLE IF EXISTS token;
-DROP TABLE IF EXISTS user_account;
+DROP TABLE IF EXISTS skills;
+DROP TABLE IF EXISTS tokens;
+DROP TABLE IF EXISTS user_accounts;
 
-CREATE TABLE skill (
+CREATE TABLE skills (
     skill_id INT GENERATED ALWAYS AS IDENTITY,
     title VARCHAR (100) NOT NULL,
     description VARCHAR (500) NOT NULL,
     location VARCHAR (100) NOT NULL,
     PRIMARY KEY (skill_id)
-    FOREIGN KEY (user_id) REFERENCES user_account(user_id)
+    FOREIGN KEY (user_id) REFERENCES user_accounts("user_id")
 );
 
-CREATE TABLE user_account (
+CREATE TABLE user_accounts (
     user_id INT GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(30) UNIQUE NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
@@ -19,12 +19,12 @@ CREATE TABLE user_account (
     PRIMARY KEY (user_id)
 );
 
-CREATE TABLE token (
+CREATE TABLE tokens (
     token_id INT GENERATED ALWAYS AS IDENTITY,
     user_id INT NOT NULL,
     token CHAR(36) UNIQUE NOT NULL,
     PRIMARY KEY (token_id),
-    FOREIGN KEY (user_id) REFERENCES user_account("user_id")
+    FOREIGN KEY (user_id) REFERENCES user_accounts("user_id")
 );
 
 -- INSERT INTO Skill VALUES (1,'Carpentry','Carpentry is a skilled trade and a craft in which the primary work performed is the cutting, shaping and installation of building materials during the construction of buildings, ships, timber bridges, concrete formwork,','Construction', 'Florin Close, Coventry, West Midlands, CV4');
