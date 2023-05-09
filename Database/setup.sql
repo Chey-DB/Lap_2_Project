742aca82d08e3ec1d282d8474eaa718afa1a926c
George Hill <georgeophill@outlook.com>
2023-05-09 12:42:00 +0100

    added date and time

--- Database/setup.sql
+++ Database/setup.sql
@@ -1,13 +1,15 @@
-DROP TABLE IF EXISTS skills;
+DROP TABLE IF EXISTS workshops;
 DROP TABLE IF EXISTS tokens;
 DROP TABLE IF EXISTS user_accounts;
 
-CREATE TABLE skills (
-    skill_id INT GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE workshops (
+    workshop_id INT GENERATED ALWAYS AS IDENTITY,
     title VARCHAR (100) NOT NULL,
     description VARCHAR (500) NOT NULL,
     location VARCHAR (100) NOT NULL,
-    PRIMARY KEY (skill_id)
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    PRIMARY KEY (workshop_id)
     FOREIGN KEY (user_id) REFERENCES user_accounts("user_id")
 );
 
@@ -26,5 +28,3 @@ CREATE TABLE tokens (
     PRIMARY KEY (token_id),
     FOREIGN KEY (user_id) REFERENCES user_accounts("user_id")
 );
-
--- INSERT INTO Skill VALUES (1,'Carpentry','Carpentry is a skilled trade and a craft in which the primary work performed is the cutting, shaping and installation of building materials during the construction of buildings, ships, timber bridges, concrete formwork,','Construction', 'Florin Close, Coventry, West Midlands, CV4');
