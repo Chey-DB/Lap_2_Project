4efc12e21363f34f75356084d3b82e3a392be419
Chey-DB <cheydb@rocketmail.com>
2023-05-10 12:15:50 +0100

    fixed bugs in setup.sql so database can setup

--- Database/setup.js
+++ Database/setup.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 const db = require('./connect');
 
-const sql = fs.readFileSync('database/setup.sql').toString();
+const sql = fs.readFileSync('./database/setup.sql').toString();
 
 db.query(sql)
   .then(data => {
     db.end();
     console.log("Database setup complete.");
   })
-  .catch(err => console.log(error));
+  .catch(error => console.log(error));
--- Database/setup.sql
+++ Database/setup.sql
@@ -2,18 +2,6 @@ DROP TABLE IF EXISTS workshops;
 DROP TABLE IF EXISTS tokens;
 DROP TABLE IF EXISTS user_accounts;
 
-CREATE TABLE workshops (
-    workshop_id INT GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR (100) NOT NULL,
-    description VARCHAR (500) NOT NULL,
-    location VARCHAR (100) NOT NULL,
-    date DATE NOT NULL,
-    time TIME NOT NULL,
-    image_data BYTEA,
-    PRIMARY KEY (workshop_id)
-    FOREIGN KEY (user_id) REFERENCES user_accounts("user_id")
-);
-
 CREATE TABLE user_accounts (
     user_id INT GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(30) UNIQUE NOT NULL,
@@ -21,6 +9,19 @@ CREATE TABLE user_accounts (
     password CHAR(60) NOT NULL,
     image_data BYTEA,
     PRIMARY KEY (user_id)
+);
+
+CREATE TABLE workshops (
+    workshop_id INT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR (100) NOT NULL,
+    description VARCHAR (500) NOT NULL,
+    location VARCHAR (100) NOT NULL,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    user_id INT NOT NULL,
+    image_data BYTEA,
+    PRIMARY KEY (workshop_id),
+    FOREIGN KEY (user_id) REFERENCES user_accounts("user_id")
 );
 
 CREATE TABLE tokens (
