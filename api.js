d8ae942d8e3b89711af300687a38f26b18b4beef
Chey-DB <cheydb@rocketmail.com>
2023-05-10 11:23:01 +0100

    created routers for users and workshops and required them in api.js

--- api.js
+++ api.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 
 const logRoutes = require('./middleware/logger');
 
+const userRouter = require('./routers/users');
+const workshopRouter = require('./routers/workshops');
+
 
 const api = express();
 
@@ -12,9 +15,15 @@ api.use(cors());
 api.use(express.json());
 api.use(logRoutes);
 
+// root route
 api.get('/', (req, res) => {
     res.send('Hello World!');
     })
+
+
+// Routers
+api.use('/users', userRouter);
+api.use('/workshops', workshopRouter);
 
 
 module.exports = api;
--- routers/users.js
+++ routers/users.js
@@ -2,14 +2,12 @@ const { Router } = require('express');
 
 const userController = require('../controllers/users');
 
-
 const userRouter = Router();
-
-
-userRouter.get('/', userController.showAllUsers);
 
 userRouter.post('/register', userController.register);
 userRouter.post('/login', userController.login);
+userRouter.get('/logout', userController.logout);
+userRouter.get('/:id', userController.viewProfile);
 
 
 module.exports = userRouter;
