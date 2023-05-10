384e14d659acea1f828846ff4b63a3539192e4c0
George Hill <georgeophill@outlook.com>
2023-05-10 10:34:13 +0100

    updated user in controllers

--- controllers/users.js
+++ controllers/users.js
@@ -3,30 +3,47 @@ const bcrypt = require('bcrypt');
 const User = require('../models/users');
 const Token = require('../models/tokens');
 
+async function register (req, res) {
+  try {
+      const data = req.body
+      const salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS))
+      data['password'] = await bcrypt.hash(data["password"], salt)
+      const result = await User.create(data)
+      res.status(201).json({
+          success: true,
+          result: result
+      })
 
-async function register (req, res ) {
-  const data = req.body;
-  const salt = await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS));
-  data.password = await bcrypt.hash(data.password, salt);
-  const result = await User.create(data);
-  res.json(result);
-}
-
-async function login (req, res ) {
-  const data = req.body;
-  const user = await User.getOneByUsername(data.username);
-  const autheniticated = await bcrypt.compare(data.password, user.password);
-  if (!autheniticated) {
-    throw new Error('Invalid credentials');
-  } else {
-    const token = await Token.create(user.user_id);
-    res.status(200).json({
-      success: true,
-      autheniticated: true,
-      token: token.token,
-    });
+  } catch (err) {
+      res.status(400).json({
+          success: false,
+          error: err
+      })
   }
+};
 
+async function login (req, res) {
+  try {
+      const data = req.body
+      const user = await User.getOneByUsername(data["username"])
+      const authenticated = await bcrypt.compare(data["password"], user["password"])
+      if (!authenticated){
+          throw new Error("Incorrect credentials")
+      } else {
+          const token = await Token.create(user["id"])
+          res.status(200).json({
+              success: true,
+              authenticated: true,
+              token: token
+          })
+      }
+
+  } catch (err) {
+      res.status(403).json({
+          success: false,
+          error: err
+      })
+  }
 }
 
 async function showAllUsers(req, res) {
